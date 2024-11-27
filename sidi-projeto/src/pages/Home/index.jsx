7425c274cd07d3d386356c56735c441610eb7a90
Renatha Alves <renathalveslima@gmail.com>
2024-11-27 01:44:57 -0300

    Second Commit

--- sidi-projeto/src/pages/Home/index.jsx
+++ sidi-projeto/src/pages/Home/index.jsx
@@ -1,14 +1,21 @@
 import './styles.css'
 
 
-const Home = () => {
-    return (
-        <div className='container-home'>
-            <h1 style={{color: '#231636', fontSize: 50}}>Home</h1>
-            <button>Sair</button>
 
-        </div>
-    )
+const Home = () => {
+  return (
+    <div className="container-home">
+      <div className="container-imagem"></div>
+      <div className="grid-container">
+        <div className="grid-item-header"></div>
+        <div className="grid-item-saldo"></div>
+        <div className="grid-item-ponto"></div>
+        <div className="grid-item-pag"></div>
+        <div className="grid-item-loc"></div>
+        <div className="grid-item-just"></div>
+      </div>
+    </div>
+  )
 }
 
 export default Home
